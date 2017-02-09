be5ed4c3406e29fdd8ff5d45a8c42ecd1cf7ea3b
Michel Grootjans <michel.grootjans@protime.eu>
2017-02-09 19:22:50 +0100

    generate json based on codes

--- generate_codes.rb
+++ generate_codes.rb
@@ -1,8 +1,3 @@
 require 'json'
-
-codes = [
-    {'qrCode': 'http://www.slingshot.company?s=1234abCD', 'shortcode': '1234abCD'},
-    {'qrCode': 'http://www.slingshot.company?s=1234abCE', 'shortcode': '1234abCE'}
-]
-
+codes = ['1234abCD', '1234abCE'].map{|code| {'qrCode': "http://www.slingshot.company?s=#{code}", 'shortcode': code} }
 File.open("qr/codes.json", "w") { |f| f.write(codes.to_json) }
